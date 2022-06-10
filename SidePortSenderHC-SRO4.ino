7914e8a1ff93bdbf94322672e24ce38e8aa9600b
Manas Pratim Biswas <75798265+sanam2405@users.noreply.github.com>
2022-06-10 14:46:28 +0530

    Create SidePortSenderHC-SRO4.ino

---
+++ SidePortSenderHC-SRO4.ino
@@ -0,0 +1,127 @@
+/*
+  Flow of the code
+  1 - Put WiFi in STA Mode
+  2 - Initialise ESPNOW
+  3 - Set Role to Combo
+  4 - Add peer device
+  5 - Define Send Callback Function
+  6 - Define Receive Callback Function
+*/
+
+#include <Wire.h>
+#ifdef ESP32
+  #include <WiFi.h>
+#else
+  #include <ESP8266WiFi.h>
+#endif
+#include <espnow.h>
+
+// REPLACE WITH THE MAC Address of your receiver
+uint8_t broadcastAddress[] = {0xC8, 0xC9, 0xA3, 0x69, 0xDE, 0x1D};
+
+const int trigPin = 12;
+const int echoPin = 14;
+
+//define sound velocity in cm/uS
+#define SOUND_VELOCITY 0.034
+#define CM_TO_INCH 0.393701
+
+long duration;
+float distanceCm;
+float distanceInch;
+
+typedef struct struct_message {
+  char msg[50];
+  float distanceInCm;
+  float distanceInInch;
+} struct_message;
+
+// Create a struct_message called outgoingReadings to hold outgoing data
+struct_message outgoingReadings;
+
+// Create a struct_message called incomingReadings to hold incoming data
+//struct_message incomingReadings;
+
+// Variable to store if sending data was successful
+String success;
+
+// Callback when data is sent
+void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
+  Serial.print("Last Packet Send Status: "); 
+  if (sendStatus == 0) {
+    Serial.println("Delivery success");
+  }
+  else {
+    Serial.println("Delivery fail");
+  }
+}
+
+// Callback when data is received
+//void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
+//  memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
+//  Serial.print("Bytes received: ");
+//  Serial.println(len);
+//  Serial.print("Data: "); Serial.println(incomingReadings.msg);
+//}
+
+
+void setup() {
+  // Init Serial Monitor
+  Serial.begin(115200);
+  
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+
+  // Init ESP-NOW
+  if (esp_now_init() != 0) {
+    Serial.println("Error initialising ESP-NOW");
+    return;
+  }
+
+  // Set ESP-NOW Role
+  esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Transmitted packet
+  esp_now_register_send_cb(OnDataSent);
+
+  // Register peer
+  esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_COMBO, 1, NULL, 0);
+
+  
+
+  // Register for a callback function that will be called when data is received
+  // esp_now_register_recv_cb(OnDataRecv);
+}
+
+void loop() {
+
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  
+  // Calculate the distance
+  distanceCm = duration * SOUND_VELOCITY/2;
+  
+  // Convert to inches
+  distanceInch = distanceCm * CM_TO_INCH;
+
+  strcpy(outgoingReadings.msg, "Hello from Side NodeMCU");
+  outgoingReadings.distanceInCm = distanceCm;
+  outgoingReadings.distanceInInch = distanceInch;
+  
+  esp_now_send(broadcastAddress, (uint8_t *) &outgoingReadings, sizeof(outgoingReadings));
+
+  delay(2000);
+}
